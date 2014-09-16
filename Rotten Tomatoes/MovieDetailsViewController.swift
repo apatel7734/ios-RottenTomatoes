4a4380f2e3b21d1d149ba5a4f206356cd4dd0b2b
Ashish <ashish@a.patel7734@gmail.com>
2014-09-16 11:18:01 -0700

    fixing UIView + Label Dragging up

--- Rotten Tomatoes/MovieDetailsViewController.swift
+++ Rotten Tomatoes/MovieDetailsViewController.swift
@@ -8,15 +8,24 @@
 
 import UIKit
 
-class MovieDetailsViewController: UIViewController {
+class MovieDetailsViewController: UIViewController, UIScrollViewDelegate{
     
     @IBOutlet weak var imageViewMoviePoster: UIImageView!
     
+    @IBOutlet weak var scrollViewMovieDetail: UIScrollView!
+    
+    @IBOutlet weak var labelMovieDetail: UILabel!
+    
+    @IBOutlet weak var uiView: UIView!
     var movieUrl :String! = ""
     var poster :NSDictionary = [:]
+    var synopsis :String = "The epic action of \"Edge of Tomorrow\" unfolds in a near future in which an alien race has hit the Earth in an unrelenting assault, unbeatable by any military unit in the world. Major William Cage (Tom Cruise) is an officer who has never seen a day of combat when he is unceremoniously dropped into what amounts to a suicide mission. Killed within minutes, Cage now finds himself inexplicably thrown into a time loop-forcing him to live out the same brutal combat over and over, fighting and dying again...and again. But with each battle, Cage becomes able to engage the adversaries with increasing skill, alongside Special Forces warrior Rita Vrataski (Emily Blunt). And, as Cage and Rita take the fight to the aliens, each repeated encounter gets them one step closer to defeating the enemy.(C) Warner Bros"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollViewMovieDetail.delegate = self
+        
         // Do any additional setup after loading the view.
         var posterUrl = poster["thumbnail"] as String
         //load low res image
@@ -26,6 +35,17 @@ class MovieDetailsViewController: UIViewController {
         //load high res image
         var origPosterUrl = posterUrl.stringByReplacingOccurrencesOfString("tmb", withString: "org", options: nil, range: nil)
         imageViewMoviePoster.setImageWithURL(NSURL(string: origPosterUrl))
+
+        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        
+        labelMovieDetail.text = synopsis
+        labelMovieDetail.sizeToFit()
+        
+        let visibleHeight = screenSize.height - uiView.frame.origin.y
+        let hiddenHeight = labelMovieDetail.frame.height - visibleHeight
+        
+        scrollViewMovieDetail.contentSize = CGSize(width: labelMovieDetail.frame.size.width, height: screenSize.height + hiddenHeight + labelMovieDetail.frame.origin.y)
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -40,4 +60,6 @@ class MovieDetailsViewController: UIViewController {
         // Pass the selected object to the new view controller.
         
     }
+    
+
 }
